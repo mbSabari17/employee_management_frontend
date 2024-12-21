c28f9d92dc6ac5706ea886aa143d2ce85cd2c933
mbSabari17 <86179452+mbSabari17@users.noreply.github.com>
2024-12-21 23:11:46 +0530

    Created the Employee Create form

---
+++ src/EmployeeCreate.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import './EmployeeCreate.css';
+
+
+const EmployeeCreate = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        position: '',
+        department: '',
+        phone: '',
+        email: '',
+      });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await fetch('http://localhost:8080/employees', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          if (response.ok) {
+            alert('Employee Created successfully');
+          } else {
+            alert(`Server error: ${response.statusText}`);
+          }
+        } catch (error) {
+          alert(`Network error: ${error.message}`);
+        }
+      };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                First Name:
+                <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <label>
+                Last Name:
+                <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <label>
+                Position:
+                <input
+                    type="text"
+                    name="position"
+                    value={formData.position}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <label>
+                Department:
+                <input
+                    type="text"
+                    name="department"
+                    value={formData.department}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <label>
+                Phone:
+                <input
+                    type="tel"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <label>
+                Email:
+                <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+            </label>
+            <button type="submit">Create</button>
+        </form>
+    );
+};
+
+export default EmployeeCreate;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import EmployeeList from './EmployeeList';
+import EmployeeCreate from './EmployeeCreate';
 
-const Navbar = () => 
-    {
-        const [showEmployeeList, setShowEmployeeList] = useState(false);
-        const handleListClick = () => {
-            setShowEmployeeList(true); // Set the state to show the employee list
-          };
+const Navbar = () => {
+  const [showEmployeeList, setShowEmployeeList] = useState(false);
+  const [ showCreate, setShowCreate ] = useState(false);
+  const handleCreateClick = () => {
+    setShowCreate(true);
+  }
+  const handleListClick = () => {
+    setShowEmployeeList(true); // Set the state to show the employee list
+  };
   return (
     <div>
-    <nav className="navbar">
-      <h1 className="navbar-title">Employee Management</h1>
-      <div className="navbar-buttons">
-      <button className="navbar-button" onClick={handleListClick}>List</button>
-        <button className="navbar-button">Create</button>
-        <button className="navbar-button">Delete</button>
-      </div>
-    </nav>
-    {showEmployeeList && <EmployeeList />}
+      <nav className="navbar">
+        <h1 className="navbar-title">Employee Management</h1>
+        <div className="navbar-buttons">
+          <button className="navbar-button" onClick={handleListClick}>List</button>
+          <button className="navbar-button" onClick={handleCreateClick}>Create</button>
+          <button className="navbar-button">Delete</button>
+        </div>
+      </nav>
+      {showEmployeeList && <EmployeeList />}
+      {showCreate && <EmployeeCreate />}
     </div>
   );
 };
